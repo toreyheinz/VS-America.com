50e583cad656a67ac1c928cce6669714e3912d2b
Torey Heinz <torey@teagles.io>
2017-10-01 21:26:07 -0400

    Saved: 2017-10-01 15:22

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,4 +3,5 @@ $(document).foundation();
 $('nav#sidebar-nav > ul').addClass('vertical menu')
 $('nav#sidebar-nav > ul ul').addClass('vertical nested menu')
 
-new Foundation.AccordionMenu($('nav#sidebar-nav > ul'), {submenuToggle: true});
+var menu = new Foundation.AccordionMenu($('nav#sidebar-nav > ul'), {submenuToggle: true});
+menu.down($('nav#sidebar-nav li.is-active ul'))
