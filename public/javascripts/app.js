2c5f357bd3eca73b71e91b3d804b049f3eba3a8b
Torey Heinz <torey@teagles.io>
2017-10-25 13:13:59 -0400

    Saved: 2017-10-24 15:58

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,6 +3,8 @@ $(document).foundation();
 $('nav#sidebar-nav > ul').addClass('vertical menu')
 $('nav#sidebar-nav > ul ul').addClass('vertical nested menu')
 
+$('nav#sidebar-subnav > ul').addClass('vertical menu')
+
 $('nav#top-menu > ul').addClass('menu')
 
 var menu = new Foundation.AccordionMenu($('nav#sidebar-nav > ul'), {submenuToggle: true});
